bead494b7ec971aa1be2ec7b198553eed4f07acb
LCarles2D <luiscarles7625@gmail.com>
2024-01-15 22:16:37 -0600

    Agregando sistema de hitbox y hurtbox, reloj completado

--- Player_mov.gd
+++ Player_mov.gd
@@ -3,11 +3,14 @@ extends CharacterBody3D
 var speed 
 const WALK_SPEED = 5.0
 const RUN_SPEED = 8.0
-const JUMP_VELOCITY = 6.0
+const JUMP_VELOCITY = 4.0 
 const SENSITIVITY = 0.002
 const FREQ = 1.75
 const AMP = 0.02
 var t_move = 0.0
+
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
@@ -48,12 +51,18 @@ func _physics_process(delta):
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
-			velocity.x = 0
-			velocity.z = 0
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.y = lerp(velocity.y, direction.y * speed, delta * 3.0)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
 	
+	#FOV 
+	var velocity_clamped = clamp(velocity.length(), 0.5, RUN_SPEED * 2.0)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camara.fov = lerp(camara.fov, target_fov, delta * 0.8)
+	
+	#Head move
 	t_move += delta * velocity.length() * float(is_on_floor())
 	camara.transform.origin = _HeadMove(t_move)
 	move_and_slide()
@@ -63,3 +72,4 @@ func _HeadMove(time):
 	pos.y = sin(time * FREQ) * AMP
 	pos.x = cos(time * FREQ / 2) * AMP
 	return pos
+
